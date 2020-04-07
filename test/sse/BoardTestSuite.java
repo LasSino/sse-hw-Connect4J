bd7613692d06203c13812ef5fd966b6f3e7a37b8
LTW <ltwsamuel@outlook.com>
2020-04-07 13:07:54 +0800

    Started path test analyze on GameController#checkWinning

---
+++ test/sse/BoardTestSuite.java
@@ -0,0 +1,9 @@
+package sse;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({BoardBasicTest.class,BoardFullTest.class})
+public class BoardTestSuite {
+}
