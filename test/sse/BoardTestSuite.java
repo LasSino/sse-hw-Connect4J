51d8ff4dde7737c06548deb5156a7727f46929be
LTW <ltwsamuel@outlook.com>
2020-04-07 17:15:05 +0800

    Renewed a new branch to totally refactor.

--- test/sse/BoardTestSuite.java
+++
@@ -1,9 +0,0 @@
-package sse;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({BoardBasicTest.class,BoardFullTest.class})
-public class BoardTestSuite {
-}
--- test/sse/GameControllerTestSuite.java
+++
@@ -1,9 +0,0 @@
-package sse;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({GameControllerCheckTest.class,GameControllerSetChessTest.class})
-public class GameControllerTestSuite {
-}
